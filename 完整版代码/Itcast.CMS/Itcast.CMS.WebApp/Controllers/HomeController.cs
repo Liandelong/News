bc82088ae5ef10e415f26aa4a9c9540efbd13e16
1147732739@qq.com <1147732739@qq.com>
2018-12-09 23:13:10 +0800

    Itcast
    MyNewSystem

---
+++ 完整版代码/Itcast.CMS/Itcast.CMS.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Itcast.CMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Itcast.CMS.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [Filter.MyAction]
+        public ActionResult Index()
+        {
+            T_UserInfo userInfo = Session["User"] as T_UserInfo;
+            ViewBag.user = userInfo.UserName;
+            return View();
+        }
+
+    }
+}
---
+++ 完整版代码/Itcast.CMS/Itcast.CMS.WebApp/Filter/MyAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Itcast.CMS.WebApp.Filter
+{
+    public class MyAction:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["User"] == null)
+            {
+                filterContext.HttpContext.Response.Redirect("/Login/Index");
+            }
+        }
+    }
+}
